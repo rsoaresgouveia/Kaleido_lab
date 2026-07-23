464951fde446dd3b837cb6bd10b1a1e4ab82e3ca
Rodrigo <developer@rodrigo.ooo>
2026-07-23 18:07:47 -0300

    🔧 chore/configure camera and ml kit for android and ios

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,8 @@ android {
         applicationId = "ooo.rodrigo.kaleido_lab"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // ML Kit face detection and CameraX require at least API 24.
+        minSdk = maxOf(flutter.minSdkVersion, 24)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
